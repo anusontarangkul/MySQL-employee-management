5759490d97f646b1112c4db5a7d46b3b02372f61
David Anusontarangkul <anusontarangkul.d@gmail.com>
2020-10-19 23:33:52 -0700

    create and link needed tables

---
+++ seed.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS employee_managementDB;
+CREATE database employee_managementDB;
+
+USE employee_managementDB;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  department_name VARCHAR(30),
+  PRIMARY KEY (id)
+);
+
+SELECT * FROM department;
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee(
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30),
+  last_name DECIMAL,
+  role_id INT,
+  manager_id INT,
+  PRIMARY KEY (id)
+);
